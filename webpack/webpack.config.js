91d1bc5b7e67a7d932b77e0ba39a39ef5cb2701a
nevtemu <artem.soroka@hotmail.com>
2021-01-16 14:08:48 +0400

    Added assets mover

--- webpack/webpack.config.js
+++ webpack/webpack.config.js
@@ -1,5 +1,6 @@
-const path = require('path')
-const HtmlWebpackPlugin = require('html-webpack-plugin')
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CopyPlugin = require("copy-webpack-plugin");
 let devMode = true;
 
 const filename = (name, ext) => devMode ? `${name}.${ext}` : `[contenthash]_${name}.${ext}`
@@ -7,21 +8,33 @@ const filename = (name, ext) => devMode ? `${name}.${ext}` : `[contenthash]_${na
 module.exports = {
     mode: 'development', 
     // mode: 'production',
+    context: path.resolve(__dirname, 'src'),
     entry: ['./script.js'],
     output: {
         filename: filename('script', 'js',),
         path: path.resolve(__dirname, 'dist')
     },
-    context: path.resolve(__dirname, 'src'),
     resolve: {
-        extensions: ['.js', '.jsx', '.ts', '.json'],
-        alias: {'@assets': path.resolve(__dirname, 'src/assets')}
-    },
+        extensions: ['.mjs', '.js', '.jsx', '.ts', '.json'],
+        alias: {
+          Assets: path.resolve(__dirname, 'assets'),
+        //   : path.resolve(__dirname, ''),
+        //   : path.resolve(__dirname, ''),
+        }
+      },
 
         plugins: [
             new HtmlWebpackPlugin({
                 template: 'index.html',
                 filename: filename('index', 'html'),
-            }) //in production mode automatically minified
-          ]
+            }), //in production mode automatically minified
+            new CopyPlugin({
+                patterns: [
+                  { from: "assets", to: "./assets/" },
+                //   { from: "", to: "" },
+                //   { from: "", to: "" },
+                ],
+              }),
+        
+        ]
 }
